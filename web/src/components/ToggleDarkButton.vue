c3a03cdcc5acef077cafa9d9f56beca39398ff05
skipeverylunch <harima.yasuhiro@outlook.jp>
2022-02-09 08:49:43 +0900

    inplemented toggle animation in ToggleDarkButton

--- web/src/components/ToggleDarkButton.vue
+++ web/src/components/ToggleDarkButton.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center">
     <div class="form-check form-switch">
       <input
-        class="float-left h-5 -ml-10 align-top bg-white bg-gray-300 bg-no-repeat bg-contain rounded-full shadow-sm appearance-none cursor-pointer  form-check-input w-9 focus:outline-none"
+        class="relative w-10 h-5 bg-gray-300 bg-no-repeat rounded-full shadow-sm appearance-none cursor-pointer  focus:outline-none"
         type="checkbox"
         role="switch"
         id="flexSwitchCheckChecked"
@@ -33,3 +33,20 @@ export default defineComponent({
   },
 });
 </script>
+<style>
+input::before {
+  position: absolute;
+  content: '';
+  width: 17px;
+  height: 17px;
+  top: 1px;
+  left: 1px;
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  transition: left 150ms;
+}
+input:checked::before {
+  left: 22px;
+}
+</style>
