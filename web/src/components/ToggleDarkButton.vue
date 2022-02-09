2e69057e27b42c4619be8bae88b12a6d131387cd
skipeverylunch <harima.yasuhiro@outlook.jp>
2022-02-10 08:52:01 +0900

    styled ToggleDarkButton

--- web/src/components/ToggleDarkButton.vue
+++ web/src/components/ToggleDarkButton.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="flex justify-center">
-    <div class="form-check form-switch">
+  <div class="flex items-center">
+    <div class="flex items-center">
+      <font-awesome-icon
+        class="mr-1 fa-lg"
+        :class="[isDarkMode ? 'text-gray-300' : 'text-blue-200']"
+        :icon="faSun"
+      />
+    </div>
+    <div class="flex items-center">
       <input
-        class="relative w-10 h-5 bg-gray-300 bg-no-repeat rounded-full shadow-sm appearance-none cursor-pointer  focus:outline-none"
+        class="
+          relative
+          w-10
+          h-5
+          bg-gray-300 bg-no-repeat
+          rounded-full
+          shadow-sm
+          appearance-none
+          cursor-pointer
+          focus:outline-none
+        "
         type="checkbox"
         role="switch"
         id="flexSwitchCheckChecked"
@@ -10,12 +27,22 @@
         @click="toggleIsDarkMode"
       />
     </div>
+    <div class="flex items-center">
+      <font-awesome-icon
+        class="ml-1 fa-lg"
+        :icon="faMoon"
+        :class="[isDarkMode ? 'text-blue-200' : 'text-gray-300']"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { ref, watch, defineComponent } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 export default defineComponent({
   name: 'ToggleDarkButton',
+  components: { FontAwesomeIcon },
   setup() {
     const isDarkMode = ref(false);
     const toggleIsDarkMode = () => {
@@ -29,6 +56,8 @@ export default defineComponent({
     return {
       isDarkMode,
       toggleIsDarkMode,
+      faSun,
+      faMoon,
     };
   },
 });
