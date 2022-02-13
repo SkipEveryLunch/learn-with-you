a84a9edb3c6552e8484096ce87852c634412a591
skipeverylunch <harima.yasuhiro@outlook.jp>
2022-02-13 18:21:41 +0900

    fixed a bug in Flag.vue

--- web/src/components/Flag.vue
+++ web/src/components/Flag.vue
@@ -10,19 +10,19 @@ export default defineComponent({
   name: 'Flag',
   props: {
     colorType: {
-      type: Object as () => ColorType,
+      type: String as () => ColorType,
       default: () => 'yellow',
       required: true,
     },
   },
   setup(props) {
     const colorClass = computed(() => {
-      console.log(props.colorType);
       switch (props.colorType) {
         case 'yellow':
           return 'text-yellow-700 bg-yellow-300';
-          break;
         case 'blue':
+          return 'text-gray-100 bg-blue-500';
+        default:
           return 'text-gray-100 bg-blue-500';
       }
     });
