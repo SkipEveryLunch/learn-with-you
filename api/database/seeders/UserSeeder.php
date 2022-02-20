914e154c4232c0cb17ab12f8e8498a38e61dac08
skipeverylunch <skipeverylunch@outlook.jp>
2022-02-20 14:48:29 +0900

    change the role of John Smith

--- api/database/seeders/UserSeeder.php
+++ api/database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
             "first_name"=>"John",
             "last_name"=>"Smith",
             "email"=>"js@test.io",
-            "role_id"=>1
+            "role_id"=>2
         ]);
         User::factory(4)->create([
             "role_id"=>2
