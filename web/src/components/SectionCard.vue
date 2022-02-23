5bfd03063666acd69b8aafbbe195bfc998e50133
skipeverylunch <skipeverylunch@outlook.jp>
2022-02-23 12:57:02 +0900

    displays series name in SectionCard

--- web/src/components/SectionCard.vue
+++ web/src/components/SectionCard.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="grid grid-cols-10 card bg-front text-normal">
-    <div class="flex items-center col-span-3 p-3">
+    <div class="flex flex-col col-span-3 p-3">
       <p class="text-2xl">{{ section.title }}</p>
+      <button class="text-xs w-max btn btn-sub-white p-1 mt-2">{{ section.series.name }}</button>
     </div>
     <div class="flex flex-col justify-center col-span-5 flex-2 wrap">
       <div>
